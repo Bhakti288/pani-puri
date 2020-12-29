ad65cdcb709105ddcf9410702dd32f0030fbea94
Bhakti288 <74050530+Bhakti288@users.noreply.github.com>
2020-12-29 11:02:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  
+  drawSprites();
+ 
+}
+
+
+
